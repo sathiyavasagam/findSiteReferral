8d18579f0628a5eb72991c2bf10144272d87b6c0
sathiyas <sathiya.vasagam@people10.com>
2017-10-29 02:27:50 +0530

    delete unused files

--- public/libs/side-menu/custom.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    var gm =new gnMenu( document.getElementById( 'gn-menu' ) );
-    $(document).click(function(event) {
-        if($(event.target).is("#custom_nav *")) return;
-        gm.bodyClickFn();
-    });
-})(jQuery);
